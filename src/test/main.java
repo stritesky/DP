79cc0c08a1a8cbdb07bda4125ebaa3af3b2c9ddf
Radek Stitesky <stritesky.radek@gmail.com>
2016-10-09 19:19:26 +0200

    1. version

--- src/test/main.java
+++ src/test/main.java
@@ -2,17 +2,26 @@ package test;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
-
-
-/**
- * Created by radek on 9.10.16.
- */
 
 public class main {
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("http://dameakci.cz").get();
+        Document doc = Jsoup.connect("https://www.sport.cz/vasenazory/222118").get();
+        Elements coments = doc.select("#discussion-block div.report-text");
+//        System.out.println(coments);
+        for (Element coment:coments
+             ) {
+
+            System.out.println(coment.select(".name").text());
+            System.out.println(coment.select(".city").text());
+            System.out.println(coment.select(".datetime").text());
+            System.out.println(coment.select("div div").text());
+        }
+
         String title = doc.title();
-        System.out.println(title);
+//        System.out.println(title);
     }
 }
